0b05c640b26a93a16345a28f8cf63aceb0461db9
banz-tenz <kesothin3@gmail.com>
2025-10-21 09:30:26 +0700

    feat:folder structure

---
+++ app.py
@@ -0,0 +1,12 @@
+# import os
+# import io
+# import zipfile
+# from flask import Flask, request, render_template, send_file, redirect, url_for
+# import pandas as pd 
+# from helpers import (
+#     generate_transcript_docx,
+#     generate_certificate_docx,
+#     generate_certificate_image,
+#     generate_transcript_docx,
+#     generate_transcript_docx,
+# )
